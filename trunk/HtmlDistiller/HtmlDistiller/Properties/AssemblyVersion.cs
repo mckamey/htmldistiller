5c1a4d8e302bb3ec93219418a31ab30fc2c5db97
Stephen <Stephen@2dd65e4e-6fdc-11de-b374-81e7cb8b6ca2>
2007-10-07 16:44:44 +0000

    - adding HtmlDistiller

---
+++ trunk/HtmlDistiller/HtmlDistiller/Properties/AssemblyVersion.cs
@@ -0,0 +1,11 @@
+/*------------------------------------------------------------------*\
+	This is auto-generated code.  All changes will be overwritten.
+\*------------------------------------------------------------------*/
+
+#region 1.0.0710.0709
+
+using System.Reflection;
+
+[assembly: AssemblyVersion("1.0.0710.0709")]
+
+#endregion
