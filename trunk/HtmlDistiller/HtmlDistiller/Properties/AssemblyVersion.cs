cb2f28ddd0ae916124dcff72d392ca206619906a
Stephen <Stephen@2dd65e4e-6fdc-11de-b374-81e7cb8b6ca2>
2008-04-15 02:52:22 +0000

    - improving HTML entity encoding & decoding - adding word break functionality to all HTML filters

--- trunk/HtmlDistiller/HtmlDistiller/Properties/AssemblyVersion.cs
+++ trunk/HtmlDistiller/HtmlDistiller/Properties/AssemblyVersion.cs
@@ -2,10 +2,10 @@
 	This is auto-generated code.  All changes will be overwritten.
 \*------------------------------------------------------------------*/
 
-#region 1.0.0801.2617
+#region 1.0.0804.1421
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.0.0801.2617")]
+[assembly: AssemblyVersion("1.0.0804.1421")]
 
 #endregion
