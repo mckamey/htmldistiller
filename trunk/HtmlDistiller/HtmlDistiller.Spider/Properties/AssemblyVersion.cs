5bb6135149fac80a4ca8f035133c288b0a325ba7
smm <smm@2dd65e4e-6fdc-11de-b374-81e7cb8b6ca2>
2009-08-15 22:56:11 +0000

    - adding HttpReferer and UserAgent headers

--- trunk/HtmlDistiller/HtmlDistiller.Spider/Properties/AssemblyVersion.cs
+++ trunk/HtmlDistiller/HtmlDistiller.Spider/Properties/AssemblyVersion.cs
@@ -2,11 +2,11 @@
 	This is auto-generated code.  All changes will be overwritten.
 \*------------------------------------------------------------------*/
 
-#region 1.0.903.1622
+#region 1.0.908.1515
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.0.903.1622")]
-[assembly: AssemblyFileVersion("1.0.903.1622")]
+[assembly: AssemblyVersion("1.0.908.1515")]
+[assembly: AssemblyFileVersion("1.0.908.1515")]
 
-#endregion 1.0.903.1622
+#endregion 1.0.908.1515
